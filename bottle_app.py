be21145b642e384cdbf998c18dec7a74ff8a98fb
raifmirza <raifmirza4@gmail.com>
2018-12-26 16:27:11 +0300

    First page

--- bottle_app.py
+++ bottle_app.py
@@ -24,14 +24,26 @@ def htmlify(head,text):
 
 def index():
     head = """ <meta charset="UTF-8">
-  <link rel="stylesheet" href="./static/style1.css"/>
-  <img class="center" src="./static/logo.png" alt="logo" widht="500px" height="450px">
-  <link rel="shortcut icon" href="./static/logo.png" type="img/x-icon">
-<title>Tarantino</title>"""
-    return htmlify("My lovely website",
-                   "This is going to be an awesome website, when it is finished.")
+                <link rel="stylesheet" href="./static/style1.css"/>
+                <img class="center" src="./static/logo.png" alt="logo" widht="500px" height="450px">
+                <link rel="shortcut icon" href="./static/logo.png" type="img/x-icon">
+                <title>Tarantino</title>"""
+    text = """<p class="q"><i>Television Actor, Producer, Film Actor, Director, Screenwriter, Actor (1963–)</i></p>
+                <br>
+                <div class="topnav">
+                <a class="active" href="index.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="films.html">Films</a>
+                </div>
+                <img src="./static/tarantino1.jpg" title="Quentin Tarantino" alt="Tarantino" width="auto" height="auto"> """
+    return htmlify(head,text)
+
+def static_file_callback(filename):
+	return static_file(filename ,root='./')
+
 
 route('/', 'GET', index)
+route('/static/<filename>' , 'GET' , static_file_callback)
 
 #####################################################################
 ### Don't alter the below code.
